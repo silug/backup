bf147402d626b2f9c1875365daf5a29c4c6e110c
steve <steve>
2001-07-11 19:00:17 +0000

    Adding script to generate host configurations.

---
+++ genhost.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl -w
+#
+# $Id: genhost.pl,v 1.1 2001/07/11 19:00:17 steve Exp $
+
+use strict;
+
+my $host=shift;
+
+if (!$host)
+{
+    print STDERR "Usage: genhost.pl <hostname>\n";
+    exit 1;
+}
+
+my @fs;
+
+open(DF, "ssh $host df |");
+
+while (<DF>)
+{
+    my @foo=split;
+    push(@fs, $foo[$#foo]) if ($foo[0]=~/^\/dev\//);
+}
+
+close(DF);
+
+for my $fs (@fs)
+{
+    print "$fs";
+    my @children=&find_children($fs, @fs);
+    if (@children)
+    {
+        print "\tDEFAULT";
+	for my $child (@children)
+	{
+	    print " --exclude $child";
+	}
+    }
+    print "\n";
+}
+
+sub find_children
+{
+    my ($fs, @fs)=@_;
+
+    my @children;
+
+    for my $child (@fs)
+    {
+	next if ($child eq $fs);
+	if ($child=~/^$fs\// or $fs eq "/")
+	{
+	    $child=~s/^$fs//;
+	    $child="/$child/";
+	    $child=~s,^//+,/,;
+	    $child=~s,//+$,/,;
+	    push(@children, $child);
+	}
+    }
+
+    for (my $n=0;$n<@children;$n++)
+    {
+	@children=grep { $_ eq $children[$n] or !/^$children[$n]/ } @children;
+    }
+
+    return @children;
+}
